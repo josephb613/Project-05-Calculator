9aee3a1c55a223eb44ea1d9512b7a77f316ae241
Joseph basix <josephbasix@gmail.com>
2023-08-17 22:38:49 +0100

    paramettre calculator

--- .lesson/instructions.md
+++ .lesson/instructions.md
@@ -1,4 +1,33 @@
- # Instructions
+# Instructions
+
+## Écrans
+
+- L'entrée du bas (_peut être sélectionnée avec la valeur d'identification_ `input`) contient le numéro de l'entrée en cours.
+- L'étiquette supérieure (_peut être sélectionnée avec la valeur d'identification_ `calcul`) contient la chaîne de calcul actuelle au fur et à mesure que l'utilisateur la tape. Les opérateurs sont précédés et suivis d'un espace.
+- Un nombre est ajouté à l'étiquette après avoir appuyé sur un opérateur.
+- Le calcul ne se fait que quand on appuie sur `=`ou `%`. `
+- Par exemple, le calcul `23 × 2 = 46`, aura `2` dans l'entrée et la chaîne `23 × 2` dans le texte au-dessus du champ de saisie.
+
+## Boutons
+
+Les boutons sont la **_SULE FAÇON_** d'entrer des données et fonctionnent de la même manière que dans une calculatrice physique, comme suit :
+
+- Une pression sur le pavé numérique permet d'ajouter un chiffre à la valeur saisie.
+- Le point `.` ne peut figurer qu'une seule fois dans une chaîne de caractères et ne peut suivre qu'un chiffre.
+- Le zéro ne peut pas être répété au début d'un nombre, par exemple `0000` n'est pas une valeur d'entrée valide.
+- En appuyant sur le bouton `+/-`, on change le signe du nombre saisi.
+- En appuyant sur le bouton `%`, le résultat du calcul est exprimé en pourcentage, c'est-à-dire qu'il est divisé par 100.
+- La touche `AC` remet tout à zéro, et la touche `C` efface l'entrée seule.
+- Pour afficher le résultat d'une opération, le signe égal `=` est ajouté à la chaîne de saisie et le résultat s'affiche dans le champ de saisie.
+
+## Bonus
+
+- ...
+- Ajouter un `00`
+-
+
+<!--
+# Instructions
 
 ## Screens
 * The bottom input (*can be selected with id value* `input`) holds the current input number.
@@ -20,4 +49,4 @@ Buttons are the ***ONLY WAY*** to input data and work as they do in a physical c
 ## Bonus
 - ...
 - Add a `00`
-- 
+- -->
--- script.js
+++ script.js
@@ -1,3 +1,139 @@
-{ calculate } import './calculator';
+let champSaisie = document.getElementById("input");
+const boutonsNumeriques = document.querySelectorAll(".numpad");
+const affichageCalcul = document.getElementById("calcul");
+const symboleEgal = document.getElementById("equals");
+const boutonPourcentage = document.getElementById("percentage");
+const boutonEffacerSaisie = document.getElementById("clear");
+const boutonReinitialiser = document.getElementById("reset");
+const boutonPlusMoins = document.getElementById("plusoumoins");
 
-// TODO: Faire la manipulation du DOM dans ce fichier
+champSaisie.value = "";
+affichageCalcul.innerText = "";
+let empecherMultiplesEgaux = 0;
+let progressionMath = 0;
+
+const touchesOperation = ['plus', 'minus', 'times', 'divideby'];
+const boutonsOperation = [];
+touchesOperation.forEach(function toucheOperation(key){
+    boutonsOperation.push(document.getElementById(key));
+});
+
+function reinitialiserDonneesSaisie() {
+    grandeListeElements = [];
+    petiteListe = [];
+}
+
+function reinitialiserToutesDonnees(event) {
+    if (affichageCalcul.innerText.includes("=")) {
+        reinitialiserDonneesSaisie();
+        champSaisie.value = "";
+        affichageCalcul.innerText = "";
+    }
+}
+
+let grandeListeElements = [];
+let petiteListe = [];
+
+function supprimerPointEtZero(tableau) {
+    let gestionPointZero = [];
+    tableau.forEach((element) => {
+        switch (element) {
+            case '0':
+                if (gestionPointZero.length >= 1 || affichageCalcul.innerText !== "") {
+                    gestionPointZero.push(element);
+                }
+                break;
+            case '.':
+                if (gestionPointZero.length >= 1 && !gestionPointZero.includes(".")) {
+                    gestionPointZero.push(".");
+                } else if (gestionPointZero.length === 0) {
+                    gestionPointZero.push("0");
+                    gestionPointZero.push(".");
+                }
+                break;
+            default:
+                gestionPointZero.push(element);
+        }
+    });
+    return gestionPointZero;
+}
+
+function effacerSaisie() {
+    reinitialiserDonneesSaisie();
+    champSaisie.value = "";
+}
+
+function effacerToutesDonnees(event) {
+    event.preventDefault();
+    reinitialiserDonneesSaisie();
+    champSaisie.value = "";
+    affichageCalcul.innerText = "";
+}
+
+function calculer() {
+    let nouveauLabel = affichageCalcul.innerText;
+    let nouvelleValeurLabel = nouveauLabel.replace("×", "*").replace("÷", "/").replace("=", "");
+    champSaisie.value = `${eval(nouvelleValeurLabel + champSaisie.value)}`;
+}
+
+function changerLabel(event, param) {
+    event.preventDefault();
+    if (affichageCalcul.innerText !== "" || champSaisie.value !== "") {
+        progressionMath += 1;
+        if (progressionMath > 1) {
+            calculer();
+        }
+        affichageCalcul.innerText = champSaisie.value + " " + param.innerText;
+    }
+    reinitialiserDonneesSaisie();
+    empecherMultiplesEgaux = 0;
+}
+
+function changerValeurSaisie(event) {
+    reinitialiserToutesDonnees();
+    if (affichageCalcul.innerText === "") {
+        grandeListeElements.push(event.target.innerText);
+        champSaisie.value = supprimerPointEtZero(grandeListeElements).join("");
+    } else {
+        petiteListe.push(event.target.innerText);
+        champSaisie.value = supprimerPointEtZero(petiteListe).join("");
+    }
+}
+
+function afficherValeurSaisie(event) {
+    event.addEventListener("click", changerValeurSaisie);
+}
+
+function afficherResultat(event) {
+    event.preventDefault();
+    empecherMultiplesEgaux += 1;
+    progressionMath = 0;
+    if (empecherMultiplesEgaux > 1) {
+        return;
+    }
+    affichageCalcul.innerText = `${affichageCalcul.innerText} ${champSaisie.value} =`;
+    champSaisie.value = "";
+    calculer();
+}
+
+function transformerEnPourcentage(event) {
+    event.preventDefault();
+    champSaisie.value = Number(champSaisie.value) / 100;
+}
+
+function basculerPlusMoins() {
+    champSaisie.value = `${champSaisie.value * (-1)}`;
+}
+
+boutonPlusMoins.addEventListener("click", basculerPlusMoins);
+boutonsNumeriques.forEach((bouton) => afficherValeurSaisie(bouton));
+boutonsOperation.forEach((operateur) => operateur.addEventListener("click", () => {
+    changerLabel(event, operateur);
+}));
+symboleEgal.addEventListener("click", afficherResultat);
+boutonPourcentage.addEventListener("click", transformerEnPourcentage);
+boutonEffacerSaisie.addEventListener("click", effacerSaisie);
+boutonReinitialiser.addEventListener("click", effacerToutesDonnees);
+
+
+
